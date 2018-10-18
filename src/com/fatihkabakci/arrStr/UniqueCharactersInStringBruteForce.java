a67ea1bc0622d2e40e5396ee18b8c9bb26f53049
Fatih <fatihkabakci@windowslive.com>
2018-10-18 11:50:07 -0700

    Brute Force

---
+++ src/com/fatihkabakci/arrStr/UniqueCharactersInStringBruteForce.java
@@ -0,0 +1,27 @@
+package com.fatihkabakci.arrStr;
+
+/**
+ * 
+ * @author fkabakci 
+ * Find out if a string has all unique characters -- Brute Force O(n2) solution
+ */
+
+public class UniqueCharactersInStringBruteForce {
+	public static boolean isUnique(String str) {
+		for (int i = 0; i < str.length(); i++) {
+			for (int j = i + 1; j < str.length(); j++) {
+				if (str.charAt(i) == str.charAt(j))
+					return false;
+			}
+		}
+		return true;
+	}
+
+	public static void main(String[] args) {
+		String computer = "Computer";
+		String printer = "Printer";
+
+		System.out.println(isUnique(computer));
+		System.out.println(isUnique(printer));
+	}
+}
